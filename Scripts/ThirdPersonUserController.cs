0988815980833f1f84a7ef80bb9aa1ef5ead370f
ymartman <eygen.martman@gmail.com>
2015-06-23 00:39:00 +0300

    User controller base script
    Reworked base third person controller

---
+++ Scripts/ThirdPersonUserController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonUserController : MonoBehaviour {
+
+	private Transform m_Cam;
+
+	// Use this for initialization
+	void Start () {
+		// get the transform of the main camera
+		if (Camera.main != null)
+		{
+			m_Cam = Camera.main.transform;
+		}
+		else
+		{
+			Debug.LogWarning(
+				"Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
